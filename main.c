815ead910840329d3d4d251770561388b660dd06
Cyraullie <cyril.goldenschue@gmail.com>
2024-11-01 16:26:00 +0100

    start normed

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
-	/* ************************************************************************** */
-	/*                                                                            */
-	/*                                                        :::      ::::::::   */
-	/*   main.c                                          :+:      :+:    :+:   */
-	/*                                                    +:+ +:+         +:+     */
-	/*   By: cgoldens <marvin@42lausanne.ch>            +#+  +:+       +#+        */
-	/*                                                +#+#+#+#+#+   +#+           */
-	/*   Created: 2024/10/28 14:44:32 by cgoldens          #+#    #+#             */
-	/*   Updated: 2024/10/28 14:44:35 by cgoldens         ###   ########.fr       */
-	/*                                                                            */
-	/* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgoldens <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 14:44:32 by cgoldens          #+#    #+#             */
+/*   Updated: 2024/10/28 14:44:35 by cgoldens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	#include "so_long.h"
-int add_floor(void *mlx, int wh, int ww, char **map, int rows, int cols)
+#include "so_long.h"
+
+int add_floor(void *mlx, int wh, int ww, t_map map)
 {
-	void *win;
-	void *img_wall, *img_floor, *img_collectible, *img_exit, *img_player;
-	int img_width, img_height;
+	void	*win;
+	void	*img_wall, *img_floor, *img_collectible, *img_exit, *img_player;
+	int	img_width, img_height;
 
 	// Charger les images correspondant à chaque type d'élément
 	img_wall = mlx_xpm_file_to_image(mlx, "textures/wall.xpm", &img_width, &img_height);
@@ -41,21 +42,21 @@ int add_floor(void *mlx, int wh, int ww, char **map, int rows, int cols)
 	}
 
 	// Afficher chaque élément de la carte en fonction de son type
-	for (int y = 0; y < rows; y++)
+	for (int y = 0; y < map.rows; y++)
 	{
-		for (int x = 0; x < cols; x++)
+		for (int x = 0; x < map.cols; x++)
 		{
 			void *current_img = NULL;
 			// Choisir l'image à afficher en fonction du caractère dans la carte
-			if (map[y][x] == '1')
+			if (map.map[y][x] == '1')
 				current_img = img_wall;
-			else if (map[y][x] == '0')
+			else if (map.map[y][x] == '0')
 				current_img = img_floor;
-			else if (map[y][x] == 'C')
+			else if (map.map[y][x] == 'C')
 				current_img = img_collectible;
-			else if (map[y][x] == 'E')
+			else if (map.map[y][x] == 'E')
 				current_img = img_exit;
-			else if (map[y][x] == 'P')
+			else if (map.map[y][x] == 'P')
 				current_img = img_player;
 
 			if (current_img)
@@ -73,91 +74,83 @@ int add_floor(void *mlx, int wh, int ww, char **map, int rows, int cols)
 	return (0);
 }
 
-char **convert_buffer_to_map(char *buffer, int *rows, int *cols)
+void	get_map_size(char *buffer, int *rows, int *cols)
 {
-	int row_count = 0;
-	int col_count = 0;
-	int i = 0;
+	int	x;
+	int	y;
+	int	i;
+	int	n;
 
-	// Compter les lignes et colonnes
-	while (buffer[i])
+	x = 0;
+	y = 1;
+	i = 0;
+	n = 0;
+	while (buffer[i] != '\0')
 	{
 		if (buffer[i] == '\n')
-			row_count++;
-		else if (row_count == 0)
-			col_count++;
+		{
+			y++;
+			n++;
+		}
+		if (n == 0)
+			x++;
 		i++;
 	}
+	*rows = y;
+	*cols = x;
+}
 
-	*rows = row_count;
-	*cols = col_count;
+char	**convert_buffer_to_map(char *buffer, int rows, int cols)
+{
+	int		row;
+	int		col;
+	int		i;
+	char	**map;
+	int		j;
 
-	// Allouer la carte en 2D
-	char **map = malloc(sizeof(char *) * row_count);
-	for (int j = 0; j < row_count; j++)
-		map[j] = malloc(sizeof(char) * (col_count + 1));
-
-	// Copier le contenu du buffer dans le tableau 2D
-	int row = 0, col = 0;
+	j = 0;
 	i = 0;
+	col = 0;
+	row = 0;
+	map = malloc(sizeof(char *) * rows);
+	while (j < rows)
+		map[j++] = malloc(sizeof(char) * (cols + 1));
 	while (buffer[i])
 	{
 		if (buffer[i] == '\n')
 		{
-			map[row][col] = '\0'; // Fin de ligne
-			row++;
+			map[row++][col] = '\0';
 			col = 0;
 		}
 		else
-		{
-			map[row][col] = buffer[i];
-			col++;
-		}
+			map[row][col++] = buffer[i];
 		i++;
 	}
-	return map;
+	return (map);
 }
 
 int	main(void)
 {
-	//void    *mlx;
-	int		fd;
+	t_map	map;
+	void	*mlx;
 	char	*buffer;
-	int y;
-	int i;
-    int rows, cols;
-    char **map;
+	int		i;
 
-	/*mlx = mlx_init();
+	mlx = mlx_init();
 	if (mlx == NULL)
-		return (1);*/
-	buffer = NULL;
-	fd = open("maps/map1.ber", O_RDONLY);
-	if (fd < 0)
-		return (1);
-	y = 0;
+		return (0);
 	i = 0;
-	buffer = read_map(fd, buffer);
-	close(fd);
-    if (!buffer)
-        return (1);
-	while (buffer[i] != '\0')
-	{
-		if (buffer[i] == '\n')
-			y++;
-		i++;
-	}
-	i--;
+	buffer = NULL;
+	buffer = open_read_map("maps/map1.ber");
+	printf("%s", buffer);
 	if (get_map_data(buffer))
 		return (0);
-	map = convert_buffer_to_map(buffer, &rows, &cols);
-    free(buffer);
-	
-
-	//add_floor(mlx, y * IMG_S, (i / y) * IMG_S, map, rows, cols);
-
-	for (int i = 0; i < rows; i++)
-		free(map[i]);
-	free(map);
-	//mlx_loop(mlx);
+	get_map_size(buffer, &map.rows, &map.cols);
+	map.map = convert_buffer_to_map(buffer, map.rows, map.cols);
+	add_floor(mlx, map.rows * IMG_S, map.cols * IMG_S, map);
+	while (i < map.rows)
+		free(map.map[i++]);
+	free(map.map);
+	mlx_loop(mlx);
+	return (0);
 }
